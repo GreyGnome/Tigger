4e7043caa3ddaf4fca48f116d8292f8d008fc6da
Mike Schwager <mike@schwager.com>
2014-07-31 13:44:04 -0500

    Changed the license to Apache-2.0.

--- Tigger.cpp
+++ Tigger.cpp
@@ -1,25 +1,24 @@
-// We use 4-character tabstops, so IN VIM:  <esc>:set ts=4
+// We use 4-character tabstops, so IN VIM:  <esc>:set ts=4 sw=4 sts=4
 // ...that's: ESCAPE key, colon key, then "s-e-t SPACE key t-s-=-4"
 /*
  * Tigger.cpp - A library for reading a switch from an Arduino digital pin.
  * Why Tigger?  "Because I'm a Tigger, and Tiggers bounce!"
  *
- * Copyright 2011 Michael Schwager (aka, "GreyGnome")
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *  Questions?  Send mail to mschwage@gmail.com
+Copyright 2011,2012,2013,2014 Michael Anthony Schwager
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+        Questions?  Send mail to mschwage@gmail.com
 */
 
 #include "Tigger.h"
